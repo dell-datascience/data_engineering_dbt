ad3888b0117102413c9b6c7a17595da3911446f4
Albert Atsu Dellor <dell.datascience@gmail.com>
2024-02-22 22:51:08 +0000

    edit fhv_facts...

--- models/staging/fhv_fact_trips.sql
+++ models/staging/fhv_fact_trips.sql
@@ -6,17 +6,16 @@ with fhv_tripdata as (
     from {{ ref('stg_fhv_tripdata') }}
 ), 
 
-dim_zones as (
+dim_zone as (
     select * 
     from {{ ref('dim_zones') }}
     where borough!= 'Unknown'
 ) 
 
-
 select *
 from fhv_tripdata
-inner join dim_zones as pickup_zone
+inner join dim_zone as pickup_zone
 on fhv_tripdata.pickup_locationid = pickup_zone.locationid
-inner join dim_zones as dropoff_zone
+inner join dim_zone as dropoff_zone
 on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
 where pickup_locationid IS NOT NULL AND dropoff_locationid IS NOT NULL
