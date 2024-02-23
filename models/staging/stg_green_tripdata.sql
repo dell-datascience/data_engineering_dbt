41cbbf81083cd21036d2ab83799be1af52e414ea
Albert Atsu Dellor <dell.datascience@gmail.com>
2024-02-23 13:41:17 +0000

    clean stg_green_tripdata

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -38,6 +38,7 @@ select
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
 from tripdata
+
 -- where rn = 1
 
 
